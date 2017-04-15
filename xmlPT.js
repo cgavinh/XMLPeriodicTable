311daea4270e5cc70e2985c571a2884086be5b18
Alex Seaver <cinaminatastic@gmail.com>
2017-04-15 18:07:43 -0400

    Now draws 113 through 118 and has an onclick response for those elements.

--- xmlPT.js
+++ xmlPT.js
@@ -10,27 +10,57 @@ $(document).ready(function()
 			var number = parseInt(numberText);
 			atoms[number]=$(this);
 		});
-		for (var i = 1; i <= 112; i++)
+		for (var i = 1; i <= 118; i++)
 		{
-			var symbol = atoms[i].find('SYMBOL').text();
-			var currentNumber = i.toString();
-			var currentElement = element + currentNumber;
-			var hashtag = "#";
-			var string = hashtag + currentElement;
-			var c = $(string)[0];
-			var ctx = c.getContext("2d");
-			ctx.font = "20px Arial";
-			if(symbol.length == 1)
+			if (i < 113)
 			{
-				ctx.fillText(symbol, 18, 33);
+				var symbol = atoms[i].find('SYMBOL').text();
+				var currentNumber = i.toString();
+				var currentElement = element + currentNumber;
+				var hashtag = "#";
+				var string = hashtag + currentElement;
+				var c = $(string)[0];
+				var ctx = c.getContext("2d");
+				ctx.font = "bold 20px Arial";
+				if(symbol.length == 1)
+				{
+					ctx.fillText(symbol, 18, 33);
+				}
+				if(symbol.length == 2)
+				{
+					ctx.fillText(symbol, 13, 33);
+				}
+				if(symbol.length == 3)
+				{
+					ctx.fillText(symbol, 8, 33);
+				}
 			}
-			if(symbol.length == 2)
+			else
 			{
-				ctx.fillText(symbol, 13, 33);
-			}
-			if(symbol.length == 3)
-			{
-				ctx.fillText(symbol, 8, 33);
+				var c = $("#element113")[0];
+				var ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uut', 8, 33);
+				c = $("#element114")[0];
+				ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uuq', 8, 33);
+				c = $("#element115")[0];
+				ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uup', 8, 33);
+				c = $("#element116")[0];
+				ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uuh', 8, 33);
+				c = $("#element117")[0];
+				ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uus', 8, 33);
+				c = $("#element118")[0];
+				ctx = c.getContext("2d");
+				ctx.font = "20px Arial";
+				ctx.fillText('Uuo', 8, 33);
 			}
 		}
 	}
@@ -54,15 +84,25 @@ $(document).ready(function()
 
 function getAtomInfo(n)
 {
-	$('#content').empty();
-	$('#content').append('<table>')
-	atoms[n].children().each(function()
+	if (n < 113)
 	{
-		nodeName = this.nodeName;
-		$("#content").append(
-			'<tr>'+ '<td>'+nodeName + ': '+'</td>'+'<td>'+$(this).text()+'</td>'+'</tr>');
-	});
-	$('#content').append('</table>')
+		$('#content').empty();
+		$('#content').append('<table>')
+		atoms[n].children().each(function()
+		{
+			nodeName = this.nodeName;
+			$("#content").append(
+				'<tr>'+ '<td>'+nodeName + ': '+'</td>'+'<td>'+$(this).text()+'</td>'+'</tr>');
+		});
+		$('#content').append('</table>')
+	}
+	else
+	{
+		$('#content').empty();
+		$('#content').append('<table>')
+		$("#content").append("Information not available for this element")
+		$('#content').append('</table>')
+	}
 }
 
 
